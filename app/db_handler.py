c0af1949d856ee7be8f40ee629c8f9636a8b81d5
Kim van Wyk <kimv@sahomeloans.com>
2019-05-14 16:43:48 +0200

    Initial commit, before 2to3ing, does not run

---
+++ app/db_handler.py
@@ -0,0 +1,447 @@
+from collections import defaultdict
+import ConfigParser
+from enum import Enum
+import operator
+
+import attr
+from sqlalchemy import create_engine, Table, MetaData, and_, or_, select
+
+from utilities import get_current_year
+
+HANDLE_EXC = True
+TABLE_PREFIX = 'md_directory'
+
+class ClubType(Enum):
+    lions = 1
+    branch = 2
+    leos = 3
+    lioness = 4
+
+@attr.s
+class MultipleDistrict(object):
+    id = attr.ib(factory=int)
+    name = attr.ib(default=None) 
+    website = attr.ib(default=None) 
+    is_in_use = attr.ib(default=False)
+    officers = attr.ib(factory=list)
+
+@attr.s
+class District(MultipleDistrict):
+    parent = attr.ib(default=None) 
+    def __attrs_post_init__(self):
+        self.name = "%s%s" % (self.parent.name if self.parent else '', self.name)
+
+@attr.s
+class Region(object):
+    id = attr.ib(factory=int)
+    name = attr.ib(default=None) 
+    chair = attr.ib(default=None) 
+    district = attr.ib(default=None)
+
+@attr.s
+class Zone(Region):
+    region = attr.ib(default=None)
+
+@attr.s
+class Club(object):
+    id = attr.ib(factory=int)
+    club_type = attr.ib(default=ClubType.lions)
+    struct = attr.ib(default=None)
+    prev_struct = attr.ib(default=None)
+    name = attr.ib(default=None)
+    meeting_time = attr.ib(default=None)
+    meeting_address = attr.ib(factory=list)
+    postal_address = attr.ib(factory=list)
+    charter_year = attr.ib(factory=int)
+    website = attr.ib(default=None)
+    is_suspended = attr.ib(default=False)
+    zone = attr.ib(default=None)
+    is_closed = attr.ib(default=False)
+    officers = attr.ib(factory=list)
+
+@attr.s
+class Member(object):
+    id = attr.ib(factory=int)
+    first_name = attr.ib(default=None)
+    last_name = attr.ib(default=None)
+    is_deceased = attr.ib(default=False)
+    is_resigned = attr.ib(default=False)
+    partner = attr.ib(default=None)
+    is_partner_lion = attr.ib(default=False)
+    join_date = attr.ib(factory=int)
+    home_ph = attr.ib(default=None)
+    bus_ph = attr.ib(default=None)
+    fax = attr.ib(default=None)
+    cell_ph = attr.ib(default=None)
+    email = attr.ib(default=None)
+    club = attr.ib(default=None)
+    title = attr.ib(default=None)
+
+    def __attrs_post_init__(self):
+        self.name = "%s %s" % (self.first_name, self.last_name)
+        if self.partner:
+            self.long_name = "%s (%s)" % (self.name, self.partner)
+        else:
+            self.long_name = self.name
+@attr.s
+class Officer(object):
+    title = attr.ib(default=None)
+    member = attr.ib(default=None)
+    committee = attr.ib(factory=list)
+
+@attr.s
+class PastOfficer(object):
+    year = attr.ib(factory=int)
+    end_month = attr.ib(default=12)
+    member = attr.ib(default=None)
+
+@attr.s
+class PastDG(PastOfficer):
+    previous_district = attr.ib(default=None)
+
+class DBHandler(object):
+    def __init__(self, username, password, schema, host, port, db_type, year=None):
+        if not year:
+            year = get_current_year()
+        self.year = year
+        engine = create_engine('%s://%s:%s@%s:%s/%s' % (db_type, username, password, host, port, schema), echo=False)
+        metadata = MetaData()
+        metadata.bind = engine
+        self.conn = engine.connect()
+        tables = [t[0] for t in self.conn.execute('SHOW TABLES').fetchall() if TABLE_PREFIX in t[0]]
+        self.tables = {}
+        for t in tables:
+            self.tables[t.split(TABLE_PREFIX)[-1].strip('_')] = Table(t, metadata, autoload=True, schema=schema)
+
+        self.officer_titles = {}
+        to = self.tables['officertitle']
+        res = self.conn.execute(to.select()).fetchall()
+        for r in res:
+            year = self.year
+            office_id = r.id
+            if r.ip_id:
+                year -= 1
+                office_id = r.ip_id
+            self.officer_titles[r.id] = (r.title, office_id, year)
+
+        self.merged_structs = defaultdict(list)
+        t = self.tables['structmerge']
+        res = self.conn.execute(t.select()).fetchall()
+        for r in res:
+            self.merged_structs[r.current_struct_id].append(r.previous_struct_id)
+
+        self.struct_ids = {}
+        t = self.tables['struct']
+        res = self.conn.execute(t.select(t.c.in_use_b == 1)).fetchall()
+        for r in res:
+            s = self.get_struct(r.id)
+            self.struct_ids[s.name] = r.id
+
+    def __db_lookup(self, lookup_id, table, mapping, exclude=[]):
+        t = self.tables[table]
+        res = self.conn.execute(t.select(t.c.id == lookup_id)).fetchone()
+        map = {}
+        for (k,v) in res.items():
+            if k not in exclude:
+                map[mapping.get(k, k)] = bool(v) if '_b' in k else v
+        return (map, res)
+
+    def __get_district_child(self, struct_id, table, order_field, getter):
+        t = self.tables[table]
+        res = db.conn.execute(t.select(t.c.struct_id == struct_id).order_by(getattr(t.c, order_field))).fetchall()
+        return [getter(r.id) for r in res]
+
+    def get_struct_list(self):
+        k = self.struct_ids.keys()
+        k.sort()
+        return k
+
+    def get_title(self, member_id, 
+                  struct_officers = ((19, 0, operator.eq, "IP"),  (19, -1, operator.eq, "PIP"),  (21, 0, operator.eq, "ID"),  
+                                     (21, -1, operator.eq, "ID"),  (11, 0, operator.eq, "CC"),  (5, 0, operator.eq, "DG"),  
+                                     (11, -1, operator.le, "PCC"),  (13, 0, operator.eq, "CCE"),  (5, -1, operator.eq, "IPDG"),  
+                                     (7, 0, operator.eq, "1VDG"),  (8, 0, operator.eq, "2VDG"),  (5, -2, operator.le, "PDG"),  
+                                     (14, 0, operator.eq, "CS"),  (15, 0, operator.eq, "CT"),  (9, 0, operator.eq, "DCS"),  
+                                     (10, 0, operator.eq, "DCT")),
+                  club_officers = ((1, 0, operator.eq, "LP"), (2, 0, operator.eq, "LS"), 
+                                   (3, 0, operator.eq, "LT"), (1, -1, operator.le, "PLP"))):
+        ''' Return a title for the supplied member_id, or None
+        if none found
+        '''
+        def search_officers(member_id, table, mapping):
+            title = None
+            t = self.tables[table]
+            res = db.conn.execute(t.select(t.c.member_id == member_id)).fetchall()
+            index = 100
+            for (n,(office_id, addition, op, ttl)) in enumerate(mapping):
+                for r in res:
+                    if all(((office_id == r.office_id), (op(r.year, self.year + addition)),
+                            (n < index))):
+                        index = n
+                        title = ttl
+                        break
+            return title
+
+        title = search_officers(member_id, 'structofficer', struct_officers)
+        if not title:
+            for (table, ttl) in (('regionchair', 'RC'), ('zonechair', 'ZC')):
+                t = self.tables[table]
+                res = db.conn.execute(t.select(and_(t.c.member_id == member_id,
+                                                    t.c.year == self.year))).fetchall()
+                if res:
+                    title = ttl
+                    break
+        if not title:
+            for (type_id, ttl) in ((1, 'MDC'), (0, 'DC')):
+                t = self.tables['struct']
+                structs = db.conn.execute(t.select(t.c.type_id == type_id)).fetchall()
+                t = self.tables['structchair']
+                for struct in structs:
+                    res = db.conn.execute(t.select(and_(t.c.member_id == member_id,
+                                                        t.c.year == self.year,
+                                                        t.c.struct_id == struct.id))).fetchall()
+                    if res:
+                        title = ttl
+                        break
+                if title:
+                    break
+        if not title:
+            title = search_officers(member_id, 'clubofficer', club_officers)
+        return title
+
+    def get_member(self, member_id, mapping={'deceased_b': 'is_deceased', 'resigned_b': 'is_resigned',
+                                             'partner_lion_b': 'is_partner_lion', 'club_id': 'club'},
+                   email=None):
+        (map, res) = self.__db_lookup(member_id, 'member', mapping)
+        map['title'] = self.get_title(member_id)
+        if email:
+            map['email'] = email
+        m = Member(**map)
+        return m
+
+    def get_club(self, club_id, mapping={'suspended_b': 'is_suspended', 'closed_b': 'is_closed',
+                                         'meet_time':'meeting_time'},
+                 exclude=('parent_id', 'struct_id', 'prev_struct_id', 'type', 'add1', 'add2', 'add3',
+                 'add4', 'add5', 'po_code', 'postal', 'postal1', 'postal2', 'postal3', 'postal4', 'postal5',
+                 'zone_id'),
+                 office_ids = {ClubType.lions: (1,2,3,4),
+                               ClubType.branch: (16,1,2,3),
+                               ClubType.lioness: (16,1,2,3),
+                               ClubType.leos: (20,1,2,3)},
+                 club_type_mapping = {0: ClubType.lions,
+                                      1: ClubType.branch,
+                                      2: ClubType.lioness,
+                                      3: ClubType.leos},
+                 include_officers=False):
+        (map, res) = self.__db_lookup(club_id, 'club', mapping, exclude)
+        map['meeting_address'] = [res['add%s' % i] for i in xrange(1,6) if res['add%s' % i]]
+        map['postal_address'] = [res['postal%s' % i] for i in xrange(1,6) if res['postal%s' % i]]
+        if res['po_code']:
+            map['postal_address'].append(res['po_code'])
+        map['club_type'] = club_type_mapping[res['type']]
+
+        if include_officers:
+            ts = self.tables['clubofficer']
+            map['officers'] = []
+            for office_id_index in office_ids[map['club_type']]:
+                (title, office_id, year) = self.officer_titles[office_id_index]
+                res = self.conn.execute(ts.select(and_(ts.c.club_id == club_id,
+                                                       ts.c.year == year,
+                                                       ts.c.office_id == office_id))).fetchone()
+                if res:
+                    map['officers'].append(Officer(title, self.get_member(res.member_id, email=res.email)))
+
+        t = self.tables['clubzone']
+        res = db.conn.execute(t.select(and_(t.c.club_id == club_id,
+                                            t.c.year == self.year))).fetchone()
+        if res:
+            map['zone'] = self.get_zone(res.zone_id)
+        
+        c = Club(**map)
+        return c
+
+    def get_region(self, region_id, exclude=('struct_id',)):
+        (map, res) = self.__db_lookup(region_id, 'region', {}, exclude)
+        map['district'] = self.get_struct(res['struct_id'])
+        t = self.tables['regionchair']
+        res = db.conn.execute(t.select(and_(t.c.parent_id == res['id'],
+                                            t.c.year == self.year))).fetchone()
+        if res:
+            map['chair'] = self.get_member(res['member_id'], email=res['email'])
+        r = Region(**map)
+        return r
+
+    def get_region_zones(self, region_id):
+        t = self.tables['zone']
+        res = db.conn.execute(t.select(and_(t.c.region_id == region_id,
+                                            t.c.in_region_b == 1)).order_by(t.c.name)).fetchall()
+        return [self.get_zone(r.id) for r in res]
+
+    def get_zone(self, zone_id, exclude=('struct_id', 'in_region_b', 'region_id')):
+        (map, res) = self.__db_lookup(zone_id, 'zone', {}, exclude)
+        map['district'] = self.get_struct(res['struct_id'])
+        if res['in_region_b']:
+            map['region'] = self.get_region(res['region_id'])
+        t = self.tables['zonechair']
+        res = db.conn.execute(t.select(and_(t.c.parent_id == res['id'],
+                                            t.c.year == self.year))).fetchone()
+        if res:
+            map['chair'] = self.get_member(res['member_id'], email=res['email'])
+        z = Zone(**map)
+        return z
+
+    def get_zone_clubs(self, zone_id):
+        t = self.tables['clubzone']
+        res = db.conn.execute(t.select(and_(t.c.zone_id == zone_id,
+                                            t.c.year == self.year))).fetchall()
+        clubs = [self.get_club(r.club_id) for r in res]
+        clubs.sort(key=lambda x:x.name)
+        return clubs
+
+    def get_struct(self, struct_id, mapping={'in_use_b': 'is_in_use'}, 
+                   class_map={0: (District, (5,6,7,8,9,10)), 
+                              1: (MultipleDistrict, (11,12,13,14,15))},
+                   exclude=('parent_id', 'type_id'),
+                   include_officers=False):
+        (map, res) = self.__db_lookup(struct_id, 'struct', mapping, exclude)
+        if res.parent_id:
+            map['parent'] = self.get_struct(res.parent_id)
+        (cls, office_ids) = class_map[res['type_id']]
+
+        if include_officers:
+            ts = self.tables['structofficer']
+            map['officers'] = []
+            for office_id_index in office_ids:
+                (title, office_id, year) = self.officer_titles[office_id_index]
+                res = self.conn.execute(ts.select(and_(ts.c.struct_id == struct_id,
+                                                       ts.c.year == year,
+                                                       ts.c.office_id == office_id))).fetchone()
+                if res:
+                    map['officers'].append(Officer(title, self.get_member(res.member_id, email=res.email)))
+            t = self.tables['structchair']
+            res = self.conn.execute(t.select(and_(t.c.struct_id == struct_id,
+                                                  t.c.year == year)).order_by(t.c.office)).fetchall()
+            if res:
+                for r in res:
+                    if r.member_id:
+                        map['officers'].append(Officer(r.office, self.get_member(r.member_id, email=r.email), 
+                                                       committee=r.committee_members.split(',') if r.committee_members else []))
+        s = cls(**map)
+        return s
+
+    def get_md_districts(self, struct_id, include_officers=False):
+        t = self.tables['struct']
+        return [self.get_struct(r.id, include_officers=include_officers) for r in db.conn.execute(t.select(and_(t.c.parent_id == struct_id, 
+                                                                                                          t.c.in_use_b == 1)).order_by(t.c.name)).fetchall()]
+
+    def get_district_clubs(self, struct_id):
+        return self.__get_district_child(struct_id, 'club', 'name', self.get_club)
+
+    def get_district_regions(self, struct_id):
+        return self.__get_district_child(struct_id, 'region', 'id', self.get_region)
+
+    def get_district_zones(self, struct_id):
+        return self.__get_district_child(struct_id, 'zone', 'id', self.get_zone)
+
+    def get_past_struct_officers(self, struct_id, office_id, cls_map={11: PastOfficer, 5: PastDG}):
+        to = self.tables['structofficer']
+        ts = self.tables['struct']
+        res = self.conn.execute(select((to.c.year, to.c.end_month, to.c.member_id, ts.c.id, ts.c.in_use_b),
+                                       and_(to.c.struct_id.in_([struct_id] + self.merged_structs.get(struct_id, [])),
+                                            to.c.office_id == office_id,
+                                            to.c.year < self.year,
+                                            to.c.struct_id == ts.c.id)).order_by(to.c.year, to.c.end_month, ts.c.name)).fetchall()
+        offs = []
+        for r in res:
+            offs.append(cls_map[office_id](r.year, r.end_month, self.get_member(r.member_id)))
+            if r.in_use_b == 0:
+                offs[-1].previous_district = self.get_struct(r.id)
+        return offs
+
+    def get_past_foreign_dgs(self, struct_id, office_id=5):
+        to = self.tables['structofficer']
+        ts = self.tables['struct']
+        tm = self.tables['member']
+        tc = self.tables['club']
+        res = self.conn.execute(select((to.c.year, to.c.end_month, to.c.member_id, ts.c.id),
+                                       and_(to.c.struct_id.notin_([struct_id] + self.merged_structs.get(struct_id, [])),
+                                            to.c.office_id == office_id,
+                                            to.c.year < self.year,
+                                            to.c.struct_id == ts.c.id,
+                                            tm.c.id == to.c.member_id,
+                                            tm.c.club_id == tc.c.id,
+                                            tc.c.struct_id == struct_id)).order_by(to.c.year, to.c.end_month, ts.c.name)).fetchall()
+        return [PastDG(r.year, r.end_month, self.get_member(r.member_id), previous_district=self.get_struct(r.id)) for r in res]
+
+    def get_past_ccs(self, struct_id):
+        return self.get_past_struct_officers(struct_id, 11)
+
+    def get_past_dgs(self, struct_id):
+        return self.get_past_struct_officers(struct_id, 5)
+
+class Data(object):
+    def __init__(self, year, struct):
+        self.db = db
+        self.struct_id = self.db.struct_ids[struct]
+        self.db.year = year
+        self.struct = self.db.get_struct(self.struct_id, include_officers=True)
+        self.__district_index = -1
+        if type(self.struct) == MultipleDistrict:
+            self.md = True
+            self.districts = self.db.get_md_districts(self.struct_id, include_officers=True)
+            self.district = None
+        else:
+            self.md = False
+            self.districts = []
+            self.district = self.struct
+
+    def next_district(self):
+        if self.md:
+            self.__district_index += 1
+            if self.__district_index >= len(self.districts):
+                return False
+            self.district = self.districts[self.__district_index]
+        return True
+        
+    def reset(self):
+        if self.md:
+            self.district = None
+            self.__district_index = -1
+
+    def get_past_ccs(self):
+        return self.db.get_past_ccs(self.struct_id)
+
+    def get_past_dgs(self):
+        if self.district:
+            return self.db.get_past_dgs(self.district.id)
+        return []
+
+    def get_district_clubs(self):
+        if self.district:
+            return self.db.get_district_clubs(self.district.id)
+        return []
+
+    def get_district_regions(self):
+        if self.district:
+            return self.db.get_district_regions(self.district.id)
+        return [] 
+
+    def get_district_zones(self):
+        if self.district:
+            return self.db.get_district_zones(self.district.id)
+        return []
+            
+def get_db_settings(fn='db_settings.ini', sec='DB'):
+    settings = {}
+    cp = ConfigParser.SafeConfigParser()
+    with open(fn, 'r') as fh:
+        cp.readfp(fh)
+    for opt in cp.options(sec):
+        settings[opt] = cp.get(sec, opt)
+    return settings
+
+def get_struct_list():
+    return db.get_struct_list()
+
+db = DBHandler(**get_db_settings())
+
---
+++ app/db_handler_test.py
@@ -0,0 +1,108 @@
+''' Tests for db_handler classes and methods
+'''
+
+from pprint import pprint
+
+import db_handler
+
+MEMBER_IDS = {'Kim': 2602128,
+       'Vicki': 2352224,
+       'Denis': 666898,
+       'Alistair': 903648,
+       'Avril': 4200750,
+       'Trevor': 709888,
+       'Phil': 2687992,
+       'Yolandi': 3762497,
+       'Barbara': 2680550,
+       'Jacqui': 1399208,
+       'Dave': 355357,
+       'Neville': 3478783,
+       'Beryl': 4040774,
+       'Lyn': 349981,
+       'Malcolm': 990112
+       }
+
+CLUB_IDS = {'North Durban': 27814,
+            'Benoni Lakes': 115092
+       }
+
+sl = db_handler.get_struct_list()
+print sl
+
+### Tests for the data class
+
+data = db_handler.Data(2019, sl[2])
+print data.struct.name
+while data.next_district():
+    print data.district.name
+data.reset()
+
+offs = []
+for off in data.struct.officers:
+    if 'Council Chairperson' == off.title:
+        offs.append(off)
+        break
+dgs = []
+vdgs = []
+while data.next_district():
+    for off in data.district.officers:
+        if off.title == 'District Governor':
+            dgs.append(off)
+        if off.title == 'First Vice District Governor':
+            vdgs.append(off)
+offs.extend(dgs)
+offs.extend(vdgs)
+for off in data.struct.officers:
+    if 'Council Chairperson' != off.title:
+        offs.append(off)
+
+pprint([(o.title, o.member.long_name) for o in offs])
+
+
+# pprint([(o.title, o.member.long_name) for o in data.struct.officers])
+# while data.next_district():
+#     pprint([(o.title, o.member.long_name) for o in data.district.officers[:2]])
+
+
+
+# pprint([(po.year, po.end_month, po.member.long_name) for po in data.get_past_ccs()][-10:])
+# data.reset()
+# data.next_district()
+# pprint([(po.year, po.end_month, po.previous_district.name, po.member.long_name) for po in data.get_past_dgs()][-10:])
+# data.reset()
+# data.next_district()
+# pprint([c.name for c in data.get_district_clubs()][-5:])
+
+### Tests for the db class
+
+# db = data.db
+# for (k,v) in MEMBER_IDS.items():
+#     print db.get_member(v)
+
+# for (k,v) in CLUB_IDS.items():
+#     print db.get_club(v, include_officers=True)
+
+# print db.get_struct(5)
+# print db.get_struct(9, include_officers=True)
+
+# pprint(db.get_md_districts(5))
+
+# print db.get_region(3)
+# print db.get_region(4)
+
+# print db.get_zone(49)
+# print db.get_zone(50)
+
+# pprint(db.get_region_zones(4))
+# pprint(db.get_zone_clubs(41))
+
+# pprint([(c.name, (c.zone.name, c.zone.region.name) if c.zone else 'No Zone') for c in db.get_district_clubs(9) if not c.is_closed])
+# pprint([r.name for r in db.get_district_regions(9)])
+# pprint([z.name for z in db.get_district_zones(9)])
+
+# pprint([(po.year, po.end_month, po.member.first_name, po.member.last_name) for po in db.get_past_ccs(5)])
+# pprint([(po.year, po.end_month, po.previous_district.name, po.member.first_name, po.member.last_name) for po in db.get_past_dgs(9)])
+
+# pprint([(po.year, po.end_month, po.previous_district.name, po.member.first_name, po.member.last_name) for po in db.get_past_foreign_dgs(9)])
+# pprint([(po.year, po.end_month, po.previous_district.name, po.member.first_name, po.member.last_name) for po in db.get_past_foreign_dgs(10)])
+
