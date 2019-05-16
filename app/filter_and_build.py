c4bab1425b8ef8354be92e608ed03eef1af79cb7
Kim van Wyk <kimv@sahomeloans.com>
2019-05-16 19:40:16 +0200

    Make function

--- app/filter_and_build.py
+++ app/filter_and_build.py
@@ -2,21 +2,22 @@
 '''
 
 import docker
-import os
+import os, os.path
 
-CONTAINER_NAME = 'pandoc'
 IMAGE_NAME = 'kimvanwyk/pandoc:latest'
-# PDF_CONTAINER_NAME = 'latex'
-# PDF_IMAGE_NAME = 'kimvanwyk/latex:latest'
 TEMPLATE='no_frills_latex.txt'
-FILE_IN='output.txt'
-client = docker.from_env()
 
-volumes = {}
-workdir = os.getcwd()
-volumes[workdir] = {'bind':'/io', 'mode':'rw'}
-client.containers.run(IMAGE_NAME, command=f'-s -o output.tex --template={TEMPLATE} {FILE_IN}', name=CONTAINER_NAME,
-                      volumes=volumes, auto_remove=True, stdin_open=True, stream=True, tty=True)
+def build_pdf(file_in, template=TEMPLATE, image=IMAGE_NAME):
+    client = docker.from_env()
+    volumes = {}
+    workdir = os.getcwd()
+    volumes[workdir] = {'bind':'/io', 'mode':'rw'}
+    (fn, ext) = os.path.splitext(file_in)
+    tex_file = f"{fn}.tex" 
+    client.containers.run(IMAGE_NAME, command=f'-s -o {tex_file} --template={template} {file_in}',
+                          volumes=volumes, auto_remove=True, stdin_open=True, stream=True, tty=True)
 
-client.containers.run(IMAGE_NAME, command=f'output.tex', name=CONTAINER_NAME, entrypoint='pdflatex',
-                      volumes=volumes, auto_remove=True, stdin_open=True, stream=True, tty=True)
+    client.containers.run(IMAGE_NAME, command=f'{tex_file}', entrypoint='pdflatex',
+                          volumes=volumes, auto_remove=True, stdin_open=True, stream=True, tty=True)
+
+build_pdf("output.txt")
