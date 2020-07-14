5c9f6d9587fa93b1e5ba177f6d728707f9704256
Kim van Wyk <kimv@sahomeloans.com>
2020-07-14 08:52:16 +0200

    Remove profile calls

--- app/db_handler.py
+++ app/db_handler.py
@@ -4,8 +4,7 @@ from enum import Enum
 import operator
 
 import attr
-from profilehooks import timecall, profile
-from sqlalchemy import create_engine, Table, MetaData, and_, or_, select
+from sqlalchemy import create_engine, Table, MetaData, and_, select
 
 from utilities import get_current_year
 
@@ -151,8 +150,6 @@ class PastDG(PastOfficer):
 
 
 class DBHandler(object):
-
-    # @profile(immediate=False, filename="profile_results")
     def __init__(self, username, password, schema, host, port, db_type, year=None):
         if not year:
             year = get_current_year()
@@ -204,7 +201,6 @@ class DBHandler(object):
         self.clubs = self.get_clubs()
         self.members = self.get_members()
 
-    # #@profile(immediate=False, filename="profile_results")
     def __db_lookup(self, lookup_id, table, mapping, exclude=[], lookup_field="id"):
         t = self.tables[table]
         res = self.conn.execute(
@@ -217,7 +213,6 @@ class DBHandler(object):
                     map[mapping.get(k, k)] = bool(v) if "_b" in k else v
         return (map, res)
 
-    # @profile(immediate=False, filename="profile_results")
     def __get_district_child(self, struct_id, table, order_field, getter, kwds={}):
         t = self.tables[table]
         res = self.conn.execute(
@@ -225,13 +220,11 @@ class DBHandler(object):
         ).fetchall()
         return [getter(r.id, **kwds) for r in res]
 
-    # @profile(immediate=False, filename="profile_results")
     def get_struct_list(self):
         k = list(self.struct_ids.keys())
         k.sort()
         return k
 
-    # @profile(immediate=False, filename="profile_results")
     def get_title(
         self,
         member_id,
@@ -265,7 +258,6 @@ class DBHandler(object):
         if none found
         """
 
-        # @profile(immediate=False, filename="profile_results")
         def search_officers(member_id, table, mapping):
             title = None
             t = self.tables[table]
@@ -319,7 +311,6 @@ class DBHandler(object):
             title = search_officers(member_id, "clubofficer", club_officers)
         return title
 
-    # @profile(immediate=False, filename="profile_results")
     def get_member(
         self,
         member_id,
@@ -371,7 +362,6 @@ class DBHandler(object):
             members[r["id"]] = Member(**map)
         return members
 
-    # @profile(immediate=False, filename="profile_results")
     def get_club(
         self,
         club_id,
@@ -481,7 +471,6 @@ class DBHandler(object):
         res = self.conn.execute(select([tc.c.id], tc.c.closed_b == False)).fetchall()
         return {r.id: self.get_club(r.id) for r in res}
 
-    # @profile(immediate=False, filename="profile_results")
     def get_region(self, region_id, exclude=("struct_id",), include_officers=False):
         (map, res) = self.__db_lookup(region_id, "region", {}, exclude)
         map["district"] = self.get_struct(
@@ -496,7 +485,6 @@ class DBHandler(object):
                 map["chair"] = self.get_member(res["member_id"], email=res["email"])
         return Region(**map)
 
-    # @profile(immediate=False, filename="profile_results")
     def get_region_zones(self, region_id, include_officers=False):
         t = self.tables["zone"]
         res = self.conn.execute(
@@ -506,7 +494,6 @@ class DBHandler(object):
         ).fetchall()
         return [self.get_zone(r.id, include_officers=include_officers) for r in res]
 
-    # @profile(immediate=False, filename="profile_results")
     def get_zone(
         self,
         zone_id,
@@ -530,7 +517,6 @@ class DBHandler(object):
                 map["chair"] = self.get_member(res["member_id"], email=res["email"])
         return Zone(**map)
 
-    # @profile(immediate=False, filename="profile_results")
     def get_zone_clubs(self, zone_id, include_officers=False):
         t = self.tables["clubzone"]
         res = self.conn.execute(
@@ -544,7 +530,6 @@ class DBHandler(object):
         clubs.sort(key=lambda x: x.name)
         return clubs
 
-    # @profile(immediate=False, filename="profile_results")
     def get_struct(
         self,
         struct_id,
@@ -599,7 +584,6 @@ class DBHandler(object):
                         )
         return cls(**map)
 
-    # @profile(immediate=False, filename="profile_results")
     def get_brightsight_offices(
         self,
         struct_id,
@@ -640,7 +624,6 @@ class DBHandler(object):
         map["struct"] = self.get_struct(struct_id)
         return BrightsightOffice(**map)
 
-    # @profile(immediate=False, filename="profile_results")
     def get_md_districts(self, struct_id, include_officers=False):
         t = self.tables["struct"]
         return [
@@ -652,7 +635,6 @@ class DBHandler(object):
             ).fetchall()
         ]
 
-    # @profile(immediate=False, filename="profile_results")
     def get_district_clubs(self, struct_id, include_officers=False):
         return self.__get_district_child(
             struct_id,
@@ -662,7 +644,6 @@ class DBHandler(object):
             {"include_officers": include_officers},
         )
 
-    # @profile(immediate=False, filename="profile_results")
     def get_district_regions(self, struct_id, include_officers=False):
         return self.__get_district_child(
             struct_id,
@@ -672,13 +653,11 @@ class DBHandler(object):
             {"include_officers": include_officers},
         )
 
-    # @profile(immediate=False, filename="profile_results")
     def get_district_zones(self, struct_id, include_officers=False):
         return self.__get_district_child(
             struct_id, "zone", "id", self.get_zone, {"include_officers": include_officers}
         )
 
-    # @profile(immediate=False, filename="profile_results")
     def get_past_struct_officers(
         self, struct_id, office_id, cls_map={11: PastOfficer, 5: PastDG}
     ):
@@ -706,7 +685,6 @@ class DBHandler(object):
                 offs[-1].previous_district = self.get_struct(r.id)
         return offs
 
-    # @profile(immediate=False, filename="profile_results")
     def get_past_foreign_dgs(self, struct_id, office_id=5):
         to = self.tables["structofficer"]
         ts = self.tables["struct"]
@@ -738,22 +716,18 @@ class DBHandler(object):
             for r in res
         ]
 
-    # @profile(immediate=False, filename="profile_results")
     def get_past_ccs(self, struct_id):
         return self.get_past_struct_officers(struct_id, 11)
 
-    # @profile(immediate=False, filename="profile_results")
     def get_past_dgs(self, struct_id):
         return self.get_past_struct_officers(struct_id, 5)
 
-    # @profile(immediate=False, filename="profile_results")
     def set_club_website(self, club_id, website):
         t = self.tables["club"]
         self.conn.execute(t.update(t.c.id == club_id, {"website": website}))
 
 
 class Data(object):
-    # @profile(immediate=False, filename="profile_results")
     def __init__(self, year, struct_name, db):
         self.db = db
         self.struct_id = self.db.struct_ids[struct_name]
@@ -775,7 +749,6 @@ class Data(object):
             self.regions = []
             self.zones = []
 
-    # @profile(immediate=False, filename="profile_results")
     def next_district(self):
         if self.md:
             self.__district_index += 1
@@ -791,7 +764,6 @@ class Data(object):
             )
         return True
 
-    # @profile(immediate=False, filename="profile_results")
     def next_region(self):
         if self.district:
             self.__region_index += 1
@@ -801,7 +773,6 @@ class Data(object):
             self.region = self.regions[self.__region_index]
         return True
 
-    # @profile(immediate=False, filename="profile_results")
     def next_zone(self):
         if self.district:
             self.__zone_index += 1
@@ -811,13 +782,11 @@ class Data(object):
             self.zone = self.zones[self.__zone_index]
         return True
 
-    # @profile(immediate=False, filename="profile_results")
     def reset(self):
         if self.md:
             self.district = None
             self.__district_index = -1
 
-    # @profile(immediate=False, filename="profile_results")
     def reset_district(self):
         if self.district:
             self.region = None
@@ -825,27 +794,22 @@ class Data(object):
             self.zone = None
             self.__zone_index = -1
 
-    # @profile(immediate=False, filename="profile_results")
     def get_past_ccs(self):
         return self.db.get_past_ccs(self.struct_id)
 
-    # @profile(immediate=False, filename="profile_results")
     def get_brightsight_offices(self):
         return self.db.get_brightsight_offices(self.struct_id)
 
-    # @profile(immediate=False, filename="profile_results")
     def get_past_dgs(self):
         if self.district:
             return self.db.get_past_dgs(self.district.id)
         return []
 
-    # @profile(immediate=False, filename="profile_results")
     def get_past_foreign_dgs(self):
         if self.district:
             return self.db.get_past_foreign_dgs(self.district.id)
         return []
 
-    # @profile(immediate=False, filename="profile_results")
     def get_district_clubs(self, include_officers=True):
         if self.district:
             return self.db.get_district_clubs(
@@ -853,13 +817,11 @@ class Data(object):
             )
         return []
 
-    # @profile(immediate=False, filename="profile_results")
     def get_district_regions(self):
         if self.district:
             return self.db.get_district_regions(self.district.id)
         return []
 
-    # @profile(immediate=False, filename="profile_results")
     def get_region_zones(self, include_officers=False):
         if self.region:
             return self.db.get_region_zones(
@@ -867,14 +829,12 @@ class Data(object):
             )
         return []
 
-    # @profile(immediate=False, filename="profile_results")
     def get_zone_clubs(self, include_officers=False):
         if self.zone:
             return self.db.get_zone_clubs(self.zone.id, include_officers=include_officers)
         return []
 
 
-# @profile(immediate=False, filename="profile_results")
 def get_db_settings(fn="db_settings.ini", sec="DB"):
     settings = {}
     cp = configparser.SafeConfigParser()
@@ -885,12 +845,10 @@ def get_db_settings(fn="db_settings.ini", sec="DB"):
     return settings
 
 
-# @profile(immediate=False, filename="profile_results")
 def get_struct_list():
     return db.get_struct_list()
 
 
-# @profile(immediate=False, filename="profile_results")
 def get_data_object_from_db(
     year, struct_name, db_settings_fn="db_settings.ini", db_settings_sec="DB"
 ):
